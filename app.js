6f05a7f0230071328bf6b0e1e127b95ec28c160f
malfus <2914801+malfus@users.noreply.github.com>
2021-07-29 15:37:14 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Please leve me alone!'));
+app.get('/', (req, res) => res.send('Please do not leave me alone!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
